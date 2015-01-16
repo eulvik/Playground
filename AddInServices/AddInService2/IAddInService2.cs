37c4ba0fcb68b52715ec1e1ed5849c2c65718d27
eirik.ulvik@gmail.com <eirik.ulvik@gmail.com>
2015-01-16 17:12:27 +0100

    Service loader shit

---
+++ AddInServices/AddInService2/IAddInService2.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace AddInService2
+{
+    [ServiceContract]
+    public interface IAddInService2
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/sayhello2")]
+        string SayHelloService2();
+    }
+}
---
+++ AddInServices/ServiceLoaderAddInView/IServiceLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.AddIn.Pipeline;
+
+namespace ServiceLoaderAddInView
+{
+    [AddInBase]
+    public interface IServiceLoader
+    {
+        bool LoadService();
+        bool StopService();
+        bool StartService();
+        bool RestartService();
+    }
+}
