50e5d820a2926dafa4bdea08a7c1f10404fcc126
eirik.ulvik@gmail.com <eirik.ulvik@gmail.com>
2015-02-03 22:52:16 +0100

    Working version

---
+++ ServiceManagementSite/scripts/controllers.js
@@ -0,0 +1,34 @@
+﻿var serviceManagementApp = angular.module('serviceManagementApp', ['ui.bootstrap']);
+
+serviceManagementApp.controller('RunningServicesCtlr', function ($scope, $http) {
+
+    $http.get("http://tpg-eiu-e6520:9008/manage/json/instances")
+        .success(function (data, status, headers, config) {
+            console.log(data);
+            $scope.services = data;
+            $scope.services.forEach(function(element, index, array) {
+                if (element.ServiceState === "Running") {
+                    element.isRunning = true;
+                } else {
+                    element.isRunning = false;
+                }
+            });
+        })
+        .error(function (data, status, headers, config) {
+            console.log(status);
+        });
+    
+    $http.get("http://tpg-eiu-e6520:9008/manage/json/addins")
+        .success(function (data, status, headers, config) {
+            console.log(data);
+            $scope.addins = data;
+        })
+        .error(function (data, status, headers, config) {
+            console.log(status);
+        });
+
+    $scope.oneAtATime = true;
+
+    $scope.hasServices = $scope.services === null;
+    $scope.hasAddins = $scope.addins === null;
+});
