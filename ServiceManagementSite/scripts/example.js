921028062f97b024f409b26fb46c5b4734e0b04a
Eirik Ulvik <eirik.ulvik@gmail.com>
2015-02-03 20:12:23 +0100

    First non working version of web site for service management

---
+++ ServiceManagementSite/scripts/example.js
@@ -0,0 +1,23 @@
+﻿var serviceManagementApp = angular.module('serviceManagementApp', ['ui.bootstrap']);
+
+serviceManagementApp.controller('RunningServicesCtlr', function ($scope, $http) {
+
+    $http.get("http://localhost:9008/manage/json/addins")
+        .success(function(response) {
+         $scope.services = response;
+    });
+    
+    $scope.oneAtATime = true;
+
+    $scope.hasServices = $scope.services === null;
+});
+
+function httpGet(theUrl)
+{
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
