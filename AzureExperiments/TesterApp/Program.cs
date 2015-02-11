90f61db4cb11b213eddc1a1aa81e39b23d28a8de
eirik.ulvik@gmail.com <eirik.ulvik@gmail.com>
2015-02-11 21:53:01 +0100

    Started working on azure storage service

---
+++ AzureExperiments/TesterApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesterApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServiceReference1.IStorageService sr = new ServiceReference1.StorageServiceClient();
+			Console.WriteLine("Performing get stuff");
+			Console.WriteLine("Result: {0}", sr.GetString("hello"));
+			Console.ReadLine();
+		}
+	}
+}
---
+++ AzureExperiments/WCFServiceWebRole1/AzureTableStorage.svc.cs
@@ -0,0 +1,26 @@
+﻿using StorageService;
+using StorageServiceImplementation;
+
+namespace AzureTableStorageWebRole
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+	public class Service1 : IStorageService
+	{
+		private AzureStorageImpl _impl;
+
+		public Service1()
+		{
+			_impl = new AzureStorageImpl();
+		}
+		public void PutString(string id, string data)
+		{
+			_impl.StoreString(id, data);
+		}
+
+		public string GetString(string id)
+		{
+			return _impl.GetString(id);
+		}
+	}
+}
